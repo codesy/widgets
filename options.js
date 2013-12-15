d9cfcb01240992db29978bd906746734b7a8f1fc
jdungan <johnadungan@gmail.com>
2013-12-15 11:31:07 -0600

    added drop down list of valid codesy domains to options page

--- options.js
+++ options.js
@@ -1,20 +1,33 @@
+var domains=['api.codesy.io','codesy-stage-herokuapp.com','codesy-dev-herokuapp.com']
+
+
 function saveOptions() {
-  var domain = document.getElementById("domain").value;
+  var domain = $('#domain_list').val();
   chrome.storage.local.set({"domain": domain});
   
   var status = document.getElementById("status");
   status.innerHTML = "Saved.";
+  
   setTimeout(function() {
     status.innerHTML = "";
   }, 750);
 }
 
+
 function loadOptions() {
+    $.each(domains,function(i,v){
+        $('#domain_list').append(
+            $('<option>')
+            .attr('value',v)
+            .text(v)
+        );
+    })
   chrome.storage.local.get(function(options){
     var domain = options.domain;
     if (!domain) { domain = "api.codesy.io"; }
-    document.getElementById("domain").value = domain;
+    $("#domain_list").val(domain);
   });
+  
 }
 
 document.addEventListener('DOMContentLoaded', loadOptions);
